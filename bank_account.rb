a8945bb9ac762573de1f500ef5cef7776e46df67
Kevin Yeh & Eli Shkurkin <pair+kyeh.shkurkin@devbootcamp.com>
2014-01-13 12:03:07 -0800

    Release 1: Add functionality

--- bank_account.rb
+++ bank_account.rb
@@ -1,9 +1,22 @@
 class BankAccount
+  attr_reader :customer_name, :type
+
   def initialize(customer_name, type, acct_number)
     @customer_name = customer_name
     @type = type
     @acct_number = acct_number
   end
+
+  def to_s
+    puts "#{customer_name}: #{type} ##{last_4_digits}"
+  end
+
+  private
+
+  def last_4_digits
+    ("*" * (@acct_number.length - 4)) + @acct_number.gsub(/-/, "")[-4..-1]
+  end
 end
 
 my_acct = BankAccount.new("Junipero Serra", "Checking", "347-923-239")
+my_acct.to_s
