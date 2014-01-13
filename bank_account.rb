3e765dc45d12cdf37a30c8d9edfda5641d5ffeeb
Christina Taggart & Matthew Higgins <pair+christina-taggart.mh120888@devbootcamp.com>
2014-01-13 12:20:08 -0800

    Add to_s method to reveal some basic account information

--- bank_account.rb
+++ bank_account.rb
@@ -3,10 +3,14 @@ class BankAccount
 def initialize(customer_name, type, acct_number)
   @customer_name = customer_name
   @type = type
-  @acct_number = acct_number
+  @acct_number = acct_number.gsub!(/-/, '')
 end
 
+def to_s
+  puts "My account information is #{@customer_name}, #{@type}, #{@acct_number.gsub(/\d{5}/, "*****")}"
+end
 
 end
 
 my_acct =  BankAccount.new("Junipero Serra", "Checking", "347-923-239")
+my_acct.to_s
