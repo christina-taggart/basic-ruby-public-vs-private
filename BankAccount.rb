6b341ed8e531310b66d6566e99611efc36ab89dd
Elliot Chalfant & Patrick Vilhena <pair+epchalfant.nifengle@devbootcamp.com>
2014-01-13 12:34:45 -0800

    Release 1: Adds to_s method

--- BankAccount.rb
+++ BankAccount.rb
@@ -1,9 +1,16 @@
 class BankAccount
+  attr_reader :acc_number. :customer_name, :type
   def initialize(customer_name, type, acc_number)
     @customer_name = customer_name
     @type = type
-    @acc_number = acc_number
+    @acc_number = "#{ "*" * (acc_number.length-4)}#{acc_number.delete("-")[-4..-1]}"
+  end
+
+  def to_s
+    "#{@customer_name}: #{@acc_number}"
   end
 end
 
-p my_acct = BankAccount.new("Junipero Serra", "Checking", "347-923-239")
+my_acct = BankAccount.new("Junipero Serra", "Checking", "347-923-239")
+
+p "My account #{my_acct}"
