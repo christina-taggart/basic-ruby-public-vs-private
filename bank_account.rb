801b834e7b5fc6c0b567b925d4bdc52b1aa46bbc
Spencer Smitherman & John Olmsted <pair+bigspencey.qsymmachus@devbootcamp.com>
2014-01-13 12:10:44 -0800

    Hide account numbers within BankAccount Class

--- bank_account.rb
+++ bank_account.rb
@@ -1,4 +1,6 @@
 class BankAccount
+  attr_accessor :customer_name
+  attr_reader :type
 
   def initialize(customer_name, type, acct_number)
     @customer_name = customer_name
@@ -6,8 +8,24 @@ class BankAccount
     @acct_number = acct_number
   end
 
+  def to_s
+    "#{@customer_name}: #{@type} # #{acct_number}"
+  end
+
+  def acct_number
+    obfuscate(@acct_number)
+  end
+
+  private
+
+  def obfuscate(number)
+    @acct_number.sub(/^\d{3}-\d{3}/, "XXX-XXX")
+  end
+
 end
 
 #-----DRIVER CODE --------
 
 my_acct = BankAccount.new("Junipero Serra", "Checking", "347-923-239")
+
+puts my_acct.to_s
