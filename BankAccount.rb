f57c8bb2e7f5f13c7b65e52fdfb4aa4b55d98923
Roy Lee & Nicholas Cu <pair+lee-sh-roy.nicholascu@devbootcamp.com>
2014-01-13 12:31:32 -0800

    Added to_s

--- BankAccount.rb
+++ BankAccount.rb
@@ -1,12 +1,24 @@
 class BankAccount
 
   def initialize(customer_name, type, acct_number)
-   @customer_name = customer_name
-   @type = type
-   @acct_number = acct_number
- end
+    @customer_name = customer_name
+    @type = type
+    @acct_number = acct_number
+  end
+
+  def to_s
+    p hidden_number = (@acct_number.delete "-").gsub(/\d{5}/,'*****')
+    "#{@customer_name}: #{@type}# #{hidden_number}"
+  end
+
+
 end
+
+
+
 
 my_acct = BankAccount.new("Junipero Serra", "Checking", "347-923-239")
 
-p BankAccount
+
+# p my_acct.to_s == "Junipero Serra: Checking# *****3239"
+p my_acct.to_s
